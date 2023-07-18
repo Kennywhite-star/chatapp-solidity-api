311560453df8be57da93a0b4faef1883683ee1f2
KEHINDE OLANIYAN <64592116+Kennywhite-star@users.noreply.github.com>
2023-07-18 01:55:50 -0700

    Add files via upload

---
+++ Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//internal import
+
+import style from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
